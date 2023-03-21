fd80bde48bf1575962239c190c100bcaff5a9a3e
Tomeno1 <51461899+Tomeno1@users.noreply.github.com>
2023-03-21 10:47:58 -0300

    update

--- springboot-cens/src/main/java/com/proyectcens/springbootcens/config/WebSecurityConfig.java
+++ springboot-cens/src/main/java/com/proyectcens/springbootcens/config/WebSecurityConfig.java
@@ -3,22 +3,24 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class WebSecurityConfig {
-    /*@Bean
-    public UserDetailsService userDetailsService() {
 
-        var user = User.withUsername("tomeno")
-                .password("tasq2801")
-                .roles("read")
-                .build();
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//
+//        var user   = User.withUsername("tomeno")
+//                .password("tasq2801")
+//                .roles("read")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(user);
+//    }
 
-        return new InMemoryUserDetailsManager(user);
-    }*/
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
+        //return NoOpPasswordEncoder.getInstance(); //No apto para producción
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
--- springboot-cens/src/main/java/com/proyectcens/springbootcens/security/SecurityUser.java
+++ springboot-cens/src/main/java/com/proyectcens/springbootcens/security/SecurityUser.java
@@ -1,12 +1,9 @@
 package com.proyectcens.springbootcens.security;
 
 import java.util.Collection;
-
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-
 import com.proyectcens.springbootcens.model.User;
-
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
@@ -16,43 +13,43 @@ public class SecurityUser implements UserDetails {
 
     @Override
     public String getUsername() {
-        // TODO Auto-generated method stub
+
         return user.getUsername();
     }
 
     @Override
     public String getPassword() {
-        // TODO Auto-generated method stub
+
         return user.getPassword();
     }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        // TODO Auto-generated method stub
+
         return user.getAuthorities().stream().map(SecurityAuthority::new).toList();
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        // TODO Auto-generated method stub
+
         return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        // TODO Auto-generated method stub
+
         return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        // TODO Auto-generated method stub
+
         return true;
     }
 
     @Override
     public boolean isEnabled() {
-        // TODO Auto-generated method stub
+
         return true;
     }
 
