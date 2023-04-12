b189a77fee50d0fccbe916451b5ab3a7d6c0ff49
Tomeno1 <51461899+Tomeno1@users.noreply.github.com>
2023-04-12 19:08:09 -0400

    add login

---
+++ springboot-cens/src/main/java/com/proyectcens/springbootcens/repositorio/UsuarioRepositorio.java
@@ -0,0 +1,13 @@
+package com.proyectcens.springbootcens.repositorio;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.proyectcens.springbootcens.modelo.Usuario;
+
+@Repository
+public interface UsuarioRepositorio extends JpaRepository<Usuario, Long>{
+
+	public Usuario findByEmail(String email);
+	
+}
