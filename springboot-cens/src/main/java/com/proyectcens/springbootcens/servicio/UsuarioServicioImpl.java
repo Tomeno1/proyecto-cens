15604752f9a9695b6c00a5a5ef2b3d6959aeaf5d
Tomeno1 <51461899+Tomeno1@users.noreply.github.com>
2023-06-12 23:31:42 -0400

    add info

--- springboot-cens/src/main/java/com/proyectcens/springbootcens/servicio/UsuarioServicioImpl.java
+++ springboot-cens/src/main/java/com/proyectcens/springbootcens/servicio/UsuarioServicioImpl.java
@@ -11,49 +11,61 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import com.proyectcens.springbootcens.controlador.dto.UsuarioRegistroDTO;
+import com.proyectcens.springbootcens.modelo.Empresa;
 import com.proyectcens.springbootcens.modelo.Rol;
 import com.proyectcens.springbootcens.modelo.RolEnum;
 import com.proyectcens.springbootcens.modelo.Usuario;
+import com.proyectcens.springbootcens.repositorio.EmpresaRepositorio;
+import com.proyectcens.springbootcens.repositorio.RolRepositorio;
 import com.proyectcens.springbootcens.repositorio.UsuarioRepositorio;
 
 @Service
 public class UsuarioServicioImpl implements UsuarioServicio {
 
     private UsuarioRepositorio usuarioRepositorio;
+    private EmpresaRepositorio empresaRepositorio;
+    private RolRepositorio rolRepositorio;
 
     @Autowired
     private BCryptPasswordEncoder passwordEncoder;
 
-    public UsuarioServicioImpl(UsuarioRepositorio usuarioRepositorio) {
+    public UsuarioServicioImpl(UsuarioRepositorio usuarioRepositorio, EmpresaRepositorio empresaRepositorio,
+            RolRepositorio rolRepositorio) {
         this.usuarioRepositorio = usuarioRepositorio;
-
+        this.empresaRepositorio = empresaRepositorio;
+        this.rolRepositorio = rolRepositorio;
     }
 
     @Override
-public Usuario guardar(UsuarioRegistroDTO registroDTO) {
-    Rol rol = new Rol(RolEnum.ROLE_ADMIN);
-    Usuario usuario = new Usuario(registroDTO.getNombre(),
-            registroDTO.getApellido(), registroDTO.getEmail(),
-            passwordEncoder.encode(registroDTO.getPassword()),
-            registroDTO.getRut(), registroDTO.getTelefono(), null, rol);
+    public Usuario guardar(Usuario registroUsuario, Empresa registroEmpresa) {
+        Rol rol = new Rol(RolEnum.ROLE_USER);
+        rol = rolRepositorio.save(rol);
+        Empresa empresa = empresaRepositorio.getById(registroEmpresa.getId());
 
-    return usuarioRepositorio.save(usuario);
-}
+        // Guardar la instancia transitoria de Empresa
 
-@Override
-public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-    Usuario usuario = usuarioRepositorio.findByEmail(username);
-    if (usuario == null) {
-        throw new UsernameNotFoundException("Usuario o contraseña inválidos");
+        // Crear el objeto Usuario con la instancia persistida de Empresa
+        Usuario usuario = new Usuario(registroUsuario.getId(), registroUsuario.getNombre(),
+                registroUsuario.getApellido(),
+                registroUsuario.getEmail(), passwordEncoder.encode(registroUsuario.getPassword()),
+                registroUsuario.getRut(), registroUsuario.getTelefono(), empresa, rol);
+
+        // Guardar el Usuario con la relación a la Empresa persistida
+        return usuarioRepositorio.save(usuario);
     }
-    return User.builder()
-            .username(usuario.getEmail())
-            .password(usuario.getPassword())
-            .authorities(mapearAutoridadesRoles(usuario.getRol().getNombre()))
-            .build();
-}
 
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Usuario usuario = usuarioRepositorio.findByEmail(username);
+        if (usuario == null) {
+            throw new UsernameNotFoundException("Usuario o contraseña inválidos");
+        }
+        return User.builder()
+                .username(usuario.getEmail())
+                .password(usuario.getPassword())
+                .authorities(mapearAutoridadesRoles(usuario.getRol().getNombre()))
+                .build();
+    }
 
     private Collection<? extends GrantedAuthority> mapearAutoridadesRoles(RolEnum rol) {
         return List.of(new SimpleGrantedAuthority(rol.name()));
@@ -63,4 +75,5 @@ public UserDetails loadUserByUsername(String username) throws UsernameNotFoundEx
     public List<Usuario> listarUsuarios() {
         return usuarioRepositorio.findAll();
     }
+
 }
